43eadc88f47ca78372a29d6704cdeac19cb73d1b
Andrés X. Vargas <axvargas@fiec.espol.edu.ec>
2021-06-13 02:10:37 -0500

    Token definition and TokenTypes Definition

---
+++ lpp/token.py
@@ -0,0 +1,30 @@
+from enum import auto, Enum, unique
+from typing import NamedTuple
+
+
+@unique
+class TokenType(Enum):
+    ASSIGN = auto()
+    COMMA = auto()
+    EOF = auto()        # ? End of file
+    FUNCTION = auto()
+    INDENT = auto()     # ? Identifier, name of variables
+    ILLEGAL = auto()    # ? Not taking part of our language
+    INT = auto()
+    LBRACE = auto()
+    LET = auto()        # ? Variable definitions
+    LPAREN = auto()
+    PLUS = auto()
+    RBRACE = auto()
+    LPAREN = auto()
+    SEMICOLON = auto()
+
+
+class Token(NamedTuple):
+    token_type: TokenType
+    literal: str
+
+    def __str__(self) -> str:
+        return f'Type: {self.token_type}, Literal: {self.literal}'
+
+    
